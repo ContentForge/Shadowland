322e0a2e3d9f46293b18f9bc3bce3436c0732fe5
qPexLegendary <terentev.andrey.2002@gmail.com>
2022-01-18 11:15:18 +0700

    Добавлена генерация дорожной разметки + генерация фонарей

--- src/apocalypse/world/object/Road.php
+++ src/apocalypse/world/object/Road.php
@@ -2,6 +2,7 @@
 
 namespace apocalypse\world\object;
 
+use apocalypse\world\ApocalypseGenerator;
 use pocketmine\block\utils\DyeColor;
 use pocketmine\block\VanillaBlocks;
 use pocketmine\utils\Random;
@@ -29,6 +30,12 @@ class Road {
         $backSide = self::BACK & $this->sides;
         $leftSide = self::LEFT & $this->sides;
 
+        $sides = 0;
+        if($forwardSide) $sides++;
+        if($rightSide) $sides++;
+        if($backSide) $sides++;
+        if($leftSide) $sides++;
+
         for($dx = 0; $dx < 16; $dx++){
             for($dz = 0; $dz < 16; $dz++){
                 $y = $chunk->getHighestBlockAt($dx, $dz);
@@ -37,36 +44,41 @@ class Road {
                 $sx = $back || $forward; $sz = $left || $right;
                 if($sx || $sz){
                     if($sx && $sz){
-                        $this->placeSidewalk($x + $dx, $y, $z + $dz, $random, $transaction);
+                        $this->placeSidewalk($x + $dx, ApocalypseGenerator::HEIGHT, $z + $dz, $random, $transaction);
                         continue;
                     }
 
                     if($forward){
-                        if($forwardSide) $this->placeRoad($x + $dx, $y, $z + $dz, $random, $transaction);
-                        else $this->placeSidewalk($x + $dx, $y, $z + $dz, $random, $transaction);
+                        if($forwardSide) $this->placeRoad($x + $dx, ApocalypseGenerator::HEIGHT, $z + $dz, $random, $transaction, $sides > 2 && $dz%2 === 0);
+                        else $this->placeSidewalk($x + $dx, ApocalypseGenerator::HEIGHT, $z + $dz, $random, $transaction);
                         continue;
                     }
 
                     if($right){
-                        if($rightSide) $this->placeRoad($x + $dx, $y, $z + $dz, $random, $transaction);
-                        else $this->placeSidewalk($x + $dx, $y, $z + $dz, $random, $transaction);
+                        if($rightSide) $this->placeRoad($x + $dx, ApocalypseGenerator::HEIGHT, $z + $dz, $random, $transaction, $sides > 2 && $dx%2 === 0);
+                        else $this->placeSidewalk($x + $dx, ApocalypseGenerator::HEIGHT, $z + $dz, $random, $transaction);
                         continue;
                     }
 
                     if($back){
-                        if($backSide) $this->placeRoad($x + $dx, $y, $z + $dz, $random, $transaction);
-                        else $this->placeSidewalk($x + $dx, $y, $z + $dz, $random, $transaction);
+                        if($backSide) $this->placeRoad($x + $dx, ApocalypseGenerator::HEIGHT, $z + $dz, $random, $transaction, $sides > 2 && $dz%2 === 0);
+                        else $this->placeSidewalk($x + $dx, ApocalypseGenerator::HEIGHT, $z + $dz, $random, $transaction);
                         continue;
                     }
 
                     if($left){
-                        if($leftSide) $this->placeRoad($x + $dx, $y, $z + $dz, $random, $transaction);
-                        else $this->placeSidewalk($x + $dx, $y, $z + $dz, $random, $transaction);
+                        if($leftSide) $this->placeRoad($x + $dx, ApocalypseGenerator::HEIGHT, $z + $dz, $random, $transaction, $sides > 2 && $dx%2 === 0);
+                        else $this->placeSidewalk($x + $dx, ApocalypseGenerator::HEIGHT, $z + $dz, $random, $transaction);
                         continue;
                     }
-                } else $this->placeRoad($x + $dx, $y, $z + $dz, $random, $transaction);
+                } else $this->placeRoad($x + $dx, ApocalypseGenerator::HEIGHT, $z + $dz, $random, $transaction);
             }
         }
+
+        if(!$forwardSide) (new RoadLamp($world, $x + 14, ApocalypseGenerator::HEIGHT + 1, $z + 7, $random, RotatableObject::BACK))->placeObject($transaction);
+        if(!$backSide) (new RoadLamp($world, $x + 1, ApocalypseGenerator::HEIGHT + 1, $z + 7, $random, RotatableObject::FORWARD))->placeObject($transaction);
+        if(!$rightSide) (new RoadLamp($world, $x + 7, ApocalypseGenerator::HEIGHT + 1, $z + 14, $random, RotatableObject::LEFT))->placeObject($transaction);
+        if(!$leftSide) (new RoadLamp($world, $x + 7, ApocalypseGenerator::HEIGHT + 1, $z + 1, $random, RotatableObject::RIGHT))->placeObject($transaction);
 
         return $transaction;
     }
@@ -80,11 +92,11 @@ class Road {
         for($dy = $y - 2; $dy < $y + 1; $dy++) $transaction->addBlockAt($x, $dy, $z, VanillaBlocks::STONE_BRICKS());
     }
 
-    private function placeRoad(int $x, int $y, int $z, Random $random, BlockTransaction $transaction){
+    private function placeRoad(int $x, int $y, int $z, Random $random, BlockTransaction $transaction, bool $print = false){
         $transaction->addBlockAt($x, $y, $z, match ($random->nextRange(0, 6)){
             1 => VanillaBlocks::COBBLESTONE(),
             2 => VanillaBlocks::COBBLESTONE_SLAB(),
-            default => VanillaBlocks::CONCRETE_POWDER()->setColor(DyeColor::GRAY()),
+            default => $print? VanillaBlocks::DIORITE() : VanillaBlocks::CONCRETE_POWDER()->setColor(DyeColor::GRAY()),
         });
         for($dy = $y - 3; $dy < $y; $dy++) $transaction->addBlockAt($x, $dy, $z, VanillaBlocks::GRAVEL());
     }
