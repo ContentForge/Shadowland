1cfc48804cd22df49e9bd9d58f71d4b8a25b4450
qPexLegendary <terentev.andrey.2002@gmail.com>
2022-02-22 17:45:42 +0700

    Добавление слушателя событий к иммерсивному таску

--- src/apocalypse/immersive/ImmersiveManager.php
+++ src/apocalypse/immersive/ImmersiveManager.php
@@ -2,7 +2,13 @@
 
 namespace apocalypse\immersive;
 
+use pocketmine\player\Player;
+
 interface ImmersiveManager {
 
     public function tick(): void;
+
+    public function onJoin(Player $player);
+
+    public function onQuit(Player $player);
 }
--- src/apocalypse/immersive/ImmersiveMechanicsTask.php
+++ src/apocalypse/immersive/ImmersiveMechanicsTask.php
@@ -2,23 +2,34 @@
 
 namespace apocalypse\immersive;
 
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\Task;
 
-class ImmersiveMechanicsTask extends Task {
+class ImmersiveMechanicsTask extends Task implements Listener {
 
-    private PluginBase $plugin;
+    /** @var ImmersiveManager[] */
     private array $managers;
 
     public function __construct(PluginBase $plugin) {
-        $this->plugin = $plugin;
-
         $this->managers = [
 
         ];
+
+        $plugin->getServer()->getPluginManager()->registerEvents($this, $plugin);
     }
 
     public function onRun(): void {
         foreach ($this->managers as $manager) $manager->tick();
+    }
+
+    public function onJoin(PlayerJoinEvent $event){
+        foreach ($this->managers as $manager) $manager->onJoin($event->getPlayer());
+    }
+
+    public function onQuit(PlayerQuitEvent $event){
+        foreach ($this->managers as $manager) $manager->onQuit($event->getPlayer());
     }
 }
