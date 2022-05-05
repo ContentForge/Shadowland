e23d6d36b6cf32ea71489d915b649d9eeccf2f76
qPexLegendary <terentev.andrey.2002@gmail.com>
2022-05-05 10:19:45 +0700

    Фунтдаментальная реализация механики жгучего солнца

--- src/apocalypse/immersive/sun/BurningSun.php
+++ src/apocalypse/immersive/sun/BurningSun.php
@@ -3,6 +3,7 @@
 namespace apocalypse\immersive\sun;
 
 use apocalypse\immersive\ImmersiveManager;
+use apocalypse\world\biome\ApocalypseBiomeManager;
 use pocketmine\block\Door;
 use pocketmine\block\Fence;
 use pocketmine\block\FenceGate;
@@ -21,10 +22,10 @@ class BurningSun implements ImmersiveManager {
     private static array $onClearSky;
     private array $players = [];
 
-    private array $ignoredBlocks;
+    private array $transparentBlocks;
 
     public function __construct() {
-        $this->ignoredBlocks = [
+        $this->transparentBlocks = [
             Glass::class,
             Thin::class,
             Wall::class,
@@ -37,16 +38,24 @@ class BurningSun implements ImmersiveManager {
 
     public function tick(): void {
         self::$onClearSky = [];
-        $server = Server::getInstance();
 
-        foreach ($server->getOnlinePlayers() as $player){
-            if(!$this->checkPosition($player)) {
-                $this->players[$player->getId()] = max($this->players[$player->getId()] - 1, 0);
-                continue;
+        foreach (Server::getInstance()->getOnlinePlayers() as $player){
+            $underSky = $this->checkPosition($player);
+            $dose = $this->players[$player->getId()] ?? 0;
+            $biome = ApocalypseBiomeManager::getInstance()->getBiome($player->getPosition());
+
+            if ($underSky) {
+                self::$onClearSky[] = $player->getId();
+                $add = $this->getDoseByTime(0.1 * ($biome === null? 1 : $biome->getSunMultiplier($player)), $player->getWorld()->getTimeOfDay());
+                $dose = min($dose + $add, 1);
+                if ($add !== 0.0) $player->sendTip("§g§l⚠ §cСолнечная радиация §g⚠");
+            } else {
+                $dose = max($dose - 0.25, 0);
             }
-            $dose = $this->players[$player->getId()];
+            $this->players[$player->getId()] = $dose;
+            if ($dose === 0.0) return;
 
-            //TODO: Эффекты
+            $biome?->handleSunEffect($player, $dose);
         }
     }
 
@@ -63,15 +72,23 @@ class BurningSun implements ImmersiveManager {
         $pos = $player->getPosition();
 
         for($y = World::Y_MAX, $pY = $pos->getY() + 1; $y > $pY; $y--){
-            $block = $world->getBlockAt((int) ($pos->getX() + 0.5), $y, (int) ($pos->getZ() + 0.5));
-            if($block->isTransparent() && !in_array($block::class, $this->ignoredBlocks)) return false;
+            $block = $world->getBlockAt((int) $pos->getX(), $y, (int) $pos->getZ());
+            if(!in_array($block::class, $this->transparentBlocks)) return false;
         }
 
-        self::$onClearSky[] = $player;
+        self::$onClearSky[] = $player->getId();
         return true;
     }
 
     public static function isUnderClearSky(Player $player): bool {
-        return in_array($player, self::$onClearSky);
+        return in_array($player->getId(), self::$onClearSky);
+    }
+
+    public function getDoseByTime(float $originalDose, int $time): float {
+        $multiplier = 1;
+
+        //TODO: Указать для каждого промежутка времени свой множитель
+
+        return $originalDose * $multiplier;
     }
 }
--- src/apocalypse/world/biome/ApocalypseBiome.php
+++ src/apocalypse/world/biome/ApocalypseBiome.php
@@ -2,6 +2,8 @@
 
 namespace apocalypse\world\biome;
 
+use pocketmine\player\Player;
+
 class ApocalypseBiome {
 
     private int $biomeId;
@@ -20,5 +22,13 @@ class ApocalypseBiome {
 
     public function getMaxRadiationLevel(): int {
         return 20;
+    }
+
+    public function getSunMultiplier(Player $player): float {
+        return 1;
+    }
+
+    public function handleSunEffect(Player $player, float $dose): void {
+        //TODO: Эффекты для каждой полученной дозы игроком
     }
 }
